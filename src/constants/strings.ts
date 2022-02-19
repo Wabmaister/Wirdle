fea261003fa791bdb13f28113446687cbad76364
Wabmaister <99593292+Wabmaister@users.noreply.github.com>
2022-02-19 10:08:21 +0100

    Update strings.ts

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,23 +1,24 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Braw!', 'Grand', 'Weel duin!']
-export const GAME_COPIED_MESSAGE = 'Gemm copied tae the clipbuird'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'No eneuch letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Wird no fund'
-export const HARD_MODE_ALERT_MESSAGE = 'Hard Mode can juist be enabelt at the stert!'
+export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
+export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
+export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const HARD_MODE_ALERT_MESSAGE =
+  'Hard Mode can only be enabled at the start!'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The wird wis ${solution}`
+  `The word was ${solution}`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
-  `Maun uise ${guess} in poseetion ${position}`
+  `Must use ${guess} in position ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess maun conteen ${letter}`
+  `Guess must contain ${letter}`
 export const ENTER_TEXT = 'Enter'
 export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Tallies'
-export const GUESS_DISTRIBUTION_TEXT = 'Shottie Spreid'
-export const NEW_WORD_TEXT = 'New wird in'
+export const STATISTICS_TITLE = 'Statistics'
+export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
+export const NEW_WORD_TEXT = 'New word in'
 export const SHARE_TEXT = 'Share'
-export const TOTAL_TRIES_TEXT = 'Total shotties'
-export const SUCCESS_RATE_TEXT = 'Win rate'
-export const CURRENT_STREAK_TEXT = 'Current rin'
-export const BEST_STREAK_TEXT = 'Best rin'
+export const TOTAL_TRIES_TEXT = 'Total tries'
+export const SUCCESS_RATE_TEXT = 'Success rate'
+export const CURRENT_STREAK_TEXT = 'Current streak'
+export const BEST_STREAK_TEXT = 'Best streak'
